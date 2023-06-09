2f346bd3ce8de97f20b68a50d31e94286d5ac809
tango1989 <jwarren2000@verizon.net>
2023-06-09 14:19:11 -0700

    Added JS Classes

--- app.js
+++ app.js
@@ -1,59 +1,97 @@
-// library !!! leave books on screen on reload, add a button for read status
-
-let myLibrary = JSON.parse(localStorage.getItem('myLibrary')) || [];
-
-const displayBooks = (myLibrary) => {
-    const bookContainer = document.getElementById('book-div');
-    bookContainer.innerHTML = '';
-
-    myLibrary.forEach((book, index) => {
-        const bookContainerElement = document.createElement('div');
-        bookContainerElement.classList.add('book-container');
-        
-        const bookElement = document.createElement('div');
-        bookElement.classList.add('book');
-
-        const titleElement = document.createElement('h3');
-        titleElement.textContent = `Title: ${book.title}`;
-        bookElement.appendChild(titleElement);
-
-        const authorElement = document.createElement('p');
-        authorElement.textContent = `Author: ${book.author}`
-        bookElement.appendChild(authorElement);
-
-        const pagesElement = document.createElement('p');
-        pagesElement.textContent = `Page count: ${book.pages}`;
-        bookElement.appendChild(pagesElement);
-        
-        const readButton = document.createElement('button');
-        readButton.textContent = book.read ? 'Read: Yes' : 'Read: No';
-        readButton.addEventListener('click', () => {
-            toggleReadStatus(index);
-        });
-        bookElement.appendChild(readButton);
-
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'X';
-        deleteButton.addEventListener('click', () => {
-            deleteBook(index)
-        });
-        bookElement.appendChild(deleteButton);
-
-        bookContainerElement.appendChild(bookElement);
-        bookContainer.appendChild(bookContainerElement);
-    });
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-};
 
 
-function toggleReadStatus(index) {
-    myLibrary[index].read = !myLibrary[index].read;
-    displayBooks(myLibrary);
+let myLibrary = [];
+
+class Book{
+     constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;     
+     }
+    info() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} `
+    };
+
 }
 
-window.addEventListener('load', () => {
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-});
+class Library {
+    constructor() {
+        this.books = [];
+    }
+    displayBooks() {
+        const bookContainer = document.getElementById('book-div');
+        bookContainer.innerHTML = '';
+    
+        this.books.forEach((book, index) => {
+            const bookContainerElement = document.createElement('div');
+            bookContainerElement.classList.add('book-container');
+            
+            const bookElement = document.createElement('div');
+            bookElement.classList.add('book');
+    
+            const titleElement = document.createElement('h3');
+            titleElement.textContent = `Title: ${book.title}`;
+            bookElement.appendChild(titleElement);
+    
+            const authorElement = document.createElement('p');
+            authorElement.textContent = `Author: ${book.author}`
+            bookElement.appendChild(authorElement);
+    
+            const pagesElement = document.createElement('p');
+            pagesElement.textContent = `Page count: ${book.pages}`;
+            bookElement.appendChild(pagesElement);
+            
+            const readButton = document.createElement('button');
+            readButton.textContent = book.read ? 'Read: Yes' : 'Read: No';
+            readButton.addEventListener('click', () => {
+                this.toggleReadStatus(index);
+            });
+            bookElement.appendChild(readButton);
+    
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'X';
+            deleteButton.addEventListener('click', () => {
+                this.deleteBook(index)
+            });
+            bookElement.appendChild(deleteButton);
+    
+            bookContainerElement.appendChild(bookElement);
+            bookContainer.appendChild(bookContainerElement);
+        });
+        localStorage.setItem('myLibrary', JSON.stringify(this.books));
+    }
+    addBook(title, author, pages, read) {
+        const newBook = new Book(title, author, pages, read);
+        this.books.push(newBook)
+        this.displayBooks()
+    }
+    
+    toggleReadStatus(index) {
+        this.books[index].read = !this.books[index].read;
+        this.displayBooks();
+    }
+    
+    deleteBook(index) {
+        this.books.splice(index, 1);
+        this.displayBooks();
+    }
+    saveLibrary() {
+        localStorage.setItem('myLibrary', JSON.stringify(this.books));
+    }
+    loadLibrary() {
+        const savedLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+        if (savedLibrary) {
+            this.books = savedLibrary.map((bookData) => {
+                const { title, author, pages, read } = bookData;
+                return new Book(title, author, pages, read);
+
+            });
+        }
+    }
+}
+
+const library = new Library();
 
 const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
@@ -65,69 +103,16 @@ form.addEventListener('submit', (e) => {
     const pages = formData.get('pages');
     const read = formData.get('read') === 'yes';
 
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-
-    displayBooks(myLibrary);
+    library.addBook(title,author,pages,read);
 
     form.reset();
-
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-});
-
-function deleteBook(index) {
-    myLibrary.splice(index, 1);
-    displayBooks(myLibrary);
-}
-
-
-// Book
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-
-    Book.prototype.info =  function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} `
-    };
-}
-document.addEventListener('DOMcontentLoaded', () => {
-    displayBooks(myLibrary);
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.addEventListener('DOMContentLoaded', () => {
+    library.loadLibrary();
+    library.displayBooks();
+});
 
 // modal
 
